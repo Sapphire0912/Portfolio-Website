3b1b3f70f8f42a1d1e0ee8b4578bebbacceb1fec
Sapphire0912 <iris228520@gmail.com>
2024-10-15 11:08:11 +0800

    Create new template

--- portfolio-website/src/app/page.tsx
+++ portfolio-website/src/app/page.tsx
@@ -1,3 +1,25 @@
+import Header from "./components/Header";
+import AboutSection from "./components/About";
+import ProjectSection from "./components/Project";
+import ResumeSection from "./components/Resume";
+import ContentSection from "./components/Content";
+
 export default function Home() {
-  return <div>This is page.tsx.</div>;
+  return (
+    <div>
+      <Header />
+      <div id="home">
+        <AboutSection />
+      </div>
+      <div id="projects">
+        <ProjectSection />
+      </div>
+      <div id="resume">
+        <ResumeSection />
+      </div>
+      <div id="content">
+        <ContentSection />
+      </div>
+    </div>
+  );
 }
