03d216fab056c226064c2ef94f02015df40338f2
Sapphire0912 <iris228520@gmail.com>
2024-10-17 07:26:44 +0800

    Create Kit Template

--- portfolio-website/src/components/Kit.tsx
+++ portfolio-website/src/components/Kit.tsx
@@ -1,12 +1,37 @@
 import React from "react";
+import Image from "next/image";
 interface DarkModeProps {
   darkMode: boolean;
 }
 
 const Kit: React.FC<DarkModeProps> = ({ darkMode }) => {
   return (
-    <section className="border-2 border-red-500 min-h-screen n">
-      <h2 className={`text-4xl text-center`}>工具箱</h2>
+    <section className="border-2 border-red-500 min-h-screen ">
+      <div className="text-center border-2 border-green-600">
+        <p className={`text-4xl`}>
+          <span
+            className={`${
+              darkMode ? "border-green-200" : "border-blue-700"
+            } border-b-4`}
+          >
+            工具箱
+          </span>
+        </p>
+
+        <div className="kit">
+          {/* 使用 grid 來排版? */}
+          <Image src="/js.png" alt="js" width={48} height={48} />
+          <Image src="/ts.png" alt="ts" width={48} height={48} />
+          <Image src="/js.png" alt="js" width={48} height={48} />
+          <Image src="/js.png" alt="js" width={48} height={48} />
+          <Image src="/js.png" alt="js" width={48} height={48} />
+          <Image src="/js.png" alt="js" width={48} height={48} />
+          <Image src="/js.png" alt="js" width={48} height={48} />
+          <Image src="/js.png" alt="js" width={48} height={48} />
+          <Image src="/js.png" alt="js" width={48} height={48} />
+          <Image src="/js.png" alt="js" width={48} height={48} />
+        </div>
+      </div>
     </section>
   );
 };
